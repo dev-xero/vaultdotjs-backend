69d63f5eea005e8f20ad2c8a4cb4730b1fafce26
= <ssh.xero@gmail.com>
2024-11-13 19:23:03 +0100

    feat: Clean exit on failed initialization

---
+++ src/database/database.provider.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import logger from '@utils/logger';
+
+/**
+ * This class manages and provides a PostgreSQL database connection.
+ * The prisma property represents a db client connection used to make database requests.
+ * Once instantiated, the provider will attempt to connect to the database -- discards
+ * the setup if it is unsuccessful.
+ */
+class DatabaseProvider {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    public get client() {
+        return this.prisma;
+    }
+
+    public async connect() {
+        await this.prisma.$connect().catch((err) => {
+            logger.error('Database connection failed.');
+            throw err;
+        });
+
+        logger.info('Successfully connected database.');
+    }
+}
+
+const databaseProvider = new DatabaseProvider();
+
+export default databaseProvider;
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,25 @@ import { startApplication } from './core/app';
 
 import generateSwaggerSpec from '@docs/swagger';
 import logger from '@utils/logger';
+import databaseProvider from './database/database.provider';
 
+// Initializes important connections such as database and redis, before starting the app.
+async function initializeConnections() {
+    try {
+        await databaseProvider.connect();
+        // await a redis connection here
+        startApplication();
+    } catch (err) {
+        logger.error(err);
+        logger.warn('Terminating.');
+    }
+}
+
+// Generate a swagger spec document before initializing
 generateSwaggerSpec()
     .catch((err) => {
         logger.error('Failed to generate swagger docs.');
         logger.error(err);
         process.exit(1);
     })
-    .then(() => startApplication());
+    .then(initializeConnections);
