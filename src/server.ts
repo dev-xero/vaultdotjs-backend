ad8c43bf8ad06127ed20e2f1ddae81c999317030
= <ssh.xero@gmail.com>
2024-11-13 13:46:01 +0100

    chore: Generate specs each time

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
-import { env } from '@config/variables';
 import { startApplication } from './core/app';
+
 import generateSwaggerSpec from '@docs/swagger';
 import logger from '@utils/logger';
 
-// generate docs each time in dev
-if (env.app.environment.isInDevelopment) {
-    generateSwaggerSpec()
-        .catch((err) => {
-            logger.error('Failed to generate swagger docs.');
-            logger.error(err);
-            process.exit(1);
-        })
-        .then(() => startApplication());
-} else {
-    startApplication();
-}
+generateSwaggerSpec()
+    .catch((err) => {
+        logger.error('Failed to generate swagger docs.');
+        logger.error(err);
+        process.exit(1);
+    })
+    .then(() => startApplication());
