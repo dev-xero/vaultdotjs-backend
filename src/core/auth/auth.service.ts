850f7109bac69cdbf726163fddd0989115ba46f9
= <ssh.xero@gmail.com>
2024-11-14 04:54:59 +0100

    feat: Store refresh token in redis cache

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import http from '@constants/http';
 import { BadRequestError } from '@errors/bad.request.error';
+import { InternalServerError } from '@errors/internal.error';
 import passwordHelper from '@helpers/password.helper';
+import tokenHelper from '@helpers/token.helper';
 import userHelper from '@helpers/user.helper';
+import redisProvider from '@providers/redis.provider';
 import logger from '@utils/logger';
+import { makeURLSafe } from '@utils/transformer';
 import { Request, Response, NextFunction } from 'express';
 
 /**
@@ -13,7 +17,7 @@ import { Request, Response, NextFunction } from 'express';
  * - Check for account duplicates via the username.
  * - If no accounts with that username exists, then create a new user account.
  * - Generate and store a new access and refresh token.
- * - The access token is stored in a redis cache for about 1hr.
+ * - The refresh token is stored in a redis cache for 2 days.
  * - Responds with the access and refresh tokens.
  *
  * @param req Request object
@@ -23,19 +27,30 @@ import { Request, Response, NextFunction } from 'express';
 export async function signup(req: Request, res: Response, next: NextFunction) {
     const { username, password } = req.body;
 
+    const urlSafeUsername = makeURLSafe(username);
     const isDuplicate = await userHelper.alreadyExists(username.toLowerCase());
 
     if (isDuplicate) throw new BadRequestError('This user already exists.');
 
+    // we will not store the passwords as plain text, instead, hash them
     const hashedPassword = passwordHelper.hash(password);
 
-    const record = await userHelper.createUser({
-        username: username.toLowerCase(),
+    await userHelper.createUser({
+        username: urlSafeUsername,
         password: hashedPassword,
     });
 
-    // generate tokens here
+    // access and refresh tokens generated here
+    const [accessToken, refreshToken] = tokenHelper.generateTokens(username);
 
+    if (!accessToken || !refreshToken) {
+        throw new InternalServerError('Failed to generate user tokens.');
+    }
+
+    await redisProvider.client.set(urlSafeUsername, refreshToken);
+    await redisProvider.client.expire(urlSafeUsername, 172800); // expires in 2 days
+
+    // request is completed
     logger.info('Successfully created new user.');
 
     return res.status(http.CREATED).json({
@@ -43,7 +58,9 @@ export async function signup(req: Request, res: Response, next: NextFunction) {
         message: 'Successfully created new user.',
         code: http.CREATED,
         data: {
-            username: record.username,
+            username: urlSafeUsername,
+            accessToken,
+            refreshToken,
         },
     });
 }
--- src/utils/transformer.ts
+++ src/utils/transformer.ts
@@ -10,3 +10,22 @@ export function extractJoiMessage(result: ValidationResult): string {
         ? `${result.error.details[0].message.replace(/"/g, '')}.`
         : '';
 }
+
+/**
+ * Removes special characters and replaces spaces to make strings
+ * URL-friendly.
+ *
+ * @param unsafe Unsafe string.
+ * @returns A url-safe string equivalent.
+ */
+export function makeURLSafe(unsafe: string): string {
+    const safeString = unsafe;
+
+    safeString
+        .toLowerCase()
+        .replace(/[^a-zA-Z ]/g, '') // removes all special characters
+        .trim()
+        .replace(/ /g, '-'); // replaces all spaces with dashes
+
+    return safeString;
+}
