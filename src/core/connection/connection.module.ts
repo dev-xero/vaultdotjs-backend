3e1a5a5454fdd7b0d9c7322906149e5d7b354ce3
= <ssh.xero@gmail.com>
2024-11-16 23:34:24 +0100

    docs: Update establish connection endpoint documentation

--- src/core/connection/connection.module.ts
+++ src/core/connection/connection.module.ts
@@ -8,7 +8,7 @@ connectionRouter.post(
     /*
         #swagger.tags = ["Connection"]
         #swagger.summary = "Establishes a database connection. 200 on successful."
-        #swagger.description = "Rate limited, this endpoint establishes a connection before users can dump or restore databases."
+        #swagger.description = "Rate limited, requires authentication, this endpoint establishes a connection before users can dump or restore databases."
         #swagger.path = "/v1/connection/establish"
     */
     rateLimited
