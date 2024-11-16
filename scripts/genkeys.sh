9f14feea2bebe2ba1cabb04700bdd452c2746151
= <ssh.xero@gmail.com>
2024-11-16 23:23:00 +0100

    sec: Key gen script + exclude generated keys from git

---
+++ scripts/genkeys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Generating Private and Public keys..."
+
+openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+openssl rsa -pubout -in private.pem -out public.pem
